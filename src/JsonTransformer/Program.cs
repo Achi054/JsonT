c0273c9317aa2cc5e07923a1b4da12dfcb0701b8
Sujith Acharya <sujithacharya@eurofins.com>
2020-03-31 21:29:06 +0530

    Updating the json config

--- src/JsonTransformer/Program.cs
+++ src/JsonTransformer/Program.cs
@@ -1,13 +1,20 @@
-﻿using ConfigTranformer.Read;
+﻿using System.Threading.Tasks;
+using ConfigTranformer.Read;
+using ConfigTranformer.Write;
 
 namespace JsonTransformer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var configPath = @"C:\TigerBox\POC\JsonTransformer\JsonTConfig.json";
-            var configurations = new Reader().FindAndDeserialize(configPath);
+            var sourcePath = @"C:\TigerBox\POC\JsonTransformer\docs\";
+            var configurations = new Reader().FindAndDeserialize(configPath).GetAwaiter().GetResult();
+            foreach (var config in configurations)
+            {
+                await new Writer().WriteToConfig(config, sourcePath);
+            }
         }
     }
 }
